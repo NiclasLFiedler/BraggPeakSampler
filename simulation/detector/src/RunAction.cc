75dd52b129b76536de8b0f750c383915962a4e48
NiclasLFiedler <niclas.fiedler@biotecmed.thm.de>
2025-09-16 06:24:36 +0200

    runaction

--- simulation/detector/src/RunAction.cc
+++ simulation/detector/src/RunAction.cc
@@ -7,7 +7,7 @@ RunAction::RunAction()
   G4RootAnalysisManager* analysisManager = G4RootAnalysisManager::Instance();
   //analysisManager->SetNtupleMerging(true);
   analysisManager->SetNtupleFileName("../data/data.root");
-  analysisManager->CreateNtuple("vtree", "track, NDet and EDep");
+  analysisManager->CreateNtuple("RawData", "track, NDet and EDep");
   analysisManager->CreateNtupleIColumn(0, "event");	
   analysisManager->CreateNtupleIColumn(0, "track");	 
   analysisManager->CreateNtupleIColumn(0, "NDet");
