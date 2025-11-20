8c159a1a906bf991a46dad7487e6f479dde2b290
NiclasLFiedler <niclas.fiedler@biotecmed.thm.de>
2025-11-20 16:39:09 +0100

    changes

--- lightyield/analyzeLightyield.py
+++ lightyield/analyzeLightyield.py
@@ -60,6 +60,7 @@ def mean_and_std_of_mean(values, std_devs):
     return mean, std_of_mean
 
 def main():        
+    showall = False
     energyNa22 = 0.511
     flatparams = np.load('data/2504/flat/output/fit_params.npy', allow_pickle=True)
     lateralparams = np.load('data/2504/lateral/output/fit_params.npy', allow_pickle=True)
@@ -100,18 +101,39 @@ def main():
 
     flat_means = np.array(flatLightyield[0])
     flat_indices = np.arange(len(flat_means))
-    thickflat_sorted_indices = np.argsort(flat_means[:15])[:12][::-1]
-    thickFlatMean = flatLightyield[0][thickflat_sorted_indices]
-    thickFlatErr = flatLightyield[2][thickflat_sorted_indices]
+    thickflat_sorted_indices = np.argsort(flat_means[:15])[::-1][:11]
     thinnflat_sorted_indices = np.argsort(flat_means[15:])[::-1] + 15
     thinnflat_sorted_indices= np.concatenate((thinnflat_sorted_indices[3:], thinnflat_sorted_indices[:3]))
 
+
+    thickFlatMean = flatLightyield[0][thickflat_sorted_indices]
+    thickFlatstddev = flatLightyield[1][thickflat_sorted_indices]
+    thickFlatErr = flatLightyield[2][thickflat_sorted_indices]
+    thickFlatstddevErr = flatLightyield[3][thickflat_sorted_indices]
+    
+    thinnFlatMean = flatLightyield[0][thinnflat_sorted_indices]
+    thinnFlatstddev = flatLightyield[1][thinnflat_sorted_indices]
+    thinnFlatErr = flatLightyield[2][thinnflat_sorted_indices]
+    thinnFlatstddeverr = flatLightyield[3][thinnflat_sorted_indices]
+    
     thinnFlatMean = flatLightyield[0][thinnflat_sorted_indices]
     thinnFlatErr = flatLightyield[2][thinnflat_sorted_indices]
-
     
+    lateral_means = np.array(lateralLightyield[0])    
+    lateral_indices = np.arange(len(lateral_means))
+    thicklateral_sorted_indices = np.argsort(lateral_means[:15])[::-1][:11]
+    thinnlateral_sorted_indices = np.argsort(lateral_means[15:])[::-1] + 15
+    thinnlateral_sorted_indices= np.concatenate((thinnlateral_sorted_indices[3:], thinnlateral_sorted_indices[:3]))
 
-    lateral_means = np.array(lateralLightyield[0])
+    thicklateralMean = lateralLightyield[0][thicklateral_sorted_indices]
+    thicklateralstddev = lateralLightyield[1][thicklateral_sorted_indices]
+    thicklateralErr = lateralLightyield[2][thicklateral_sorted_indices]
+    thicklateralstddeverr = lateralLightyield[3][thicklateral_sorted_indices]
+    
+    thinnlateralMean = lateralLightyield[0][thinnlateral_sorted_indices]
+    thinnlateralstddev = lateralLightyield[1][thinnlateral_sorted_indices]
+    thinnlateralErr = lateralLightyield[2][thinnlateral_sorted_indices]
+    thinnlateralstddeverr = lateralLightyield[3][thinnlateral_sorted_indices]
 
     first15_flat_mean = np.mean(flat_means[:15])
     last21_flat_mean = np.mean(flat_means[-21:])
@@ -122,38 +144,76 @@ def main():
     last21_lateral_mean = np.mean(lateral_means[-21:])
     first15_lateral_std = np.std(lateral_means[:15], ddof=1)
     last21_lateral_std = np.std(lateral_means[-21:], ddof=1)
-
-    plt.rcParams.update({'font.size': 22})
-    plt.errorbar(range(12), thickFlatMean, yerr=thickFlatErr, fmt='o', color = "orange", capsize=5, label='Mean ± error')
-    plt.errorbar(range(12,33), thinnFlatMean, yerr=thinnFlatErr, fmt='o', color = "orange", capsize=5)
-
-    print(thickflat_sorted_indices)
-    print(thinnflat_sorted_indices)
-
-    plt.show()
-    plt.errorbar(range(len(flatLightyield[0])), flatLightyield[0], yerr=flatLightyield[1], fmt='none',linewidth = 3, ecolor='#FFD580',capsize=5, label='Std. dev.')
-
-    plt.errorbar(range(len(lateralLightyield[0])), lateralLightyield[0], yerr=lateralLightyield[2], fmt='o', color = "green", capsize=5, label='Mean ± error')
-    plt.errorbar(range(len(lateralLightyield[0])), lateralLightyield[0], yerr=lateralLightyield[1], fmt='none',linewidth = 3, ecolor='#90EE90',capsize=5,  label='Std. dev.')
-
-    plt.text(-1, first15_flat_mean-3, f'{first15_flat_mean:.2f} \n' f'$\\pm${first15_flat_std:.2f}', color='dodgerblue', fontsize=16, va='bottom', ha='center')
-    plt.text(36, last21_flat_mean-3, f'{last21_flat_mean:.2f}\n' f'$\\pm${last21_flat_std:.2f}', color='dodgerblue', fontsize=16, va='bottom', ha='center')
-
-    plt.text(-1, first15_lateral_mean-3, f'{first15_lateral_mean:.2f}\n' f'$\\pm${first15_lateral_std:.2f}', color='crimson', fontsize=16,va='bottom', ha='center')
-    plt.text(36, last21_lateral_mean-3, f'{last21_lateral_mean:.2f}\n' f'$\\pm${last21_lateral_std:.2f}', color='crimson', fontsize=16, va='bottom', ha='center')
-
-    plt.plot(range(15), [first15_flat_mean] * 15, color='dodgerblue', linestyle='--')
-    plt.plot(range(15, 36), [last21_flat_mean] * 21, color='dodgerblue', linestyle='--')
-
-    plt.plot(range(15), [first15_lateral_mean] * 15, color='crimson', linestyle='--') 
-    plt.plot(range(15, 36), [last21_lateral_mean] * 21, color='crimson', linestyle='--')
     
-    plt.xlabel('Crystal index')
-    plt.ylabel('Light yield / ph/MeV')
-    plt.legend()
-    plt.grid(True)
-    plt.tight_layout()
-    plt.show()
     
+    thickflataverage = np.mean(thickFlatMean)
+    thinnflataverage = np.mean(thinnFlatMean)
+    thickflatstd = np.std(thickFlatMean, ddof=1)
+    thinnflatstd = np.std(thinnFlatMean, ddof=1)
+
+    thicklateralaverage = np.mean(thicklateralMean)
+    thinnlateralaverage = np.mean(thinnlateralMean)
+    thicklateralstd = np.std(thicklateralMean, ddof=1)
+    thinnlateralstd = np.std(thinnlateralMean, ddof=1)
+
+    plt.rcParams.update({'font.size': 28})
+    plt.figure(figsize=(18, 15))
+    
+    if(showall):
+        plt.errorbar(range(len(flatLightyield[0])), flatLightyield[0], yerr=flatLightyield[1], fmt='none',linewidth = 3, ecolor='#FFD580',capsize=5, label='Std. dev.')
+
+        plt.errorbar(range(len(lateralLightyield[0])), lateralLightyield[0], yerr=lateralLightyield[2], fmt='o', color = "green", capsize=5, label='Mean ± error')
+        plt.errorbar(range(len(lateralLightyield[0])), lateralLightyield[0], yerr=lateralLightyield[1], fmt='none',linewidth = 3, ecolor='#90EE90',capsize=5,  label='Std. dev.')
+
+        plt.text(-1, first15_flat_mean-3, f'{first15_flat_mean:.2f} \n' f'$\\pm${first15_flat_std:.2f}', color='dodgerblue', fontsize=16, va='bottom', ha='center')
+        plt.text(36, last21_flat_mean-3, f'{last21_flat_mean:.2f}\n' f'$\\pm${last21_flat_std:.2f}', color='dodgerblue', fontsize=16, va='bottom', ha='center')
+
+        plt.text(-1, first15_lateral_mean-3, f'{first15_lateral_mean:.2f}\n' f'$\\pm${first15_lateral_std:.2f}', color='crimson', fontsize=16,va='bottom', ha='center')
+        plt.text(36, last21_lateral_mean-3, f'{last21_lateral_mean:.2f}\n' f'$\\pm${last21_lateral_std:.2f}', color='crimson', fontsize=16, va='bottom', ha='center')
+
+        plt.plot(range(15), [first15_flat_mean] * 15, color='dodgerblue', linestyle='--')
+        plt.plot(range(15, 36), [last21_flat_mean] * 21, color='dodgerblue', linestyle='--')
+
+        plt.plot(range(15), [first15_lateral_mean] * 15, color='crimson', linestyle='--') 
+        plt.plot(range(15, 36), [last21_lateral_mean] * 21, color='crimson', linestyle='--')
+        
+        plt.xlabel('Crystal index')
+        plt.ylabel('Light yield / ph/MeV')
+        plt.legend()
+        plt.grid(True)
+        plt.tight_layout()
+        plt.show()
+    else:
+        plt.errorbar(range(len(thickFlatMean)), thickFlatMean, yerr=thickFlatErr, fmt='o', color = "orange", markersize=8, capsize=8, label='Flat: Mean ± error')
+        
+        plt.errorbar(range(len(thickFlatMean)), thickFlatMean, yerr=thickFlatstddev, fmt='none',linewidth = 6, ecolor='#FFD580',capsize=8, label='Flat: Std. dev.')
+
+        plt.errorbar(range(len(thickFlatMean), len(thickFlatMean)+len(thinnFlatMean)), thinnFlatMean, yerr=thinnFlatErr, fmt='o', color = "orange", markersize=8,  capsize=8)
+        
+        plt.errorbar(range(len(thickFlatMean), len(thickFlatMean)+len(thinnFlatMean)), thinnFlatMean, yerr=thinnFlatstddev, fmt='none',linewidth = 6, ecolor='#FFD580',capsize=8)
+
+
+        plt.errorbar(range(len(thicklateralMean)), thicklateralMean, yerr=thicklateralErr, fmt='o', color = "green", markersize=8, capsize=8, label='Vertical: Mean ± error')
+        
+        plt.errorbar(range(len(thicklateralMean)), thicklateralMean, yerr=thicklateralstddev, fmt='none',linewidth = 6, ecolor='#90EE90',capsize=8, label='Vertical: Std. dev.')
+
+        plt.errorbar(range(len(thicklateralMean), len(thicklateralMean)+len(thinnlateralMean)), thinnlateralMean, yerr=thinnlateralErr, fmt='o', color = "green", markersize=8, capsize=8)
+        
+        plt.errorbar(range(len(thicklateralMean), len(thicklateralMean)+len(thinnlateralMean)), thinnlateralMean, yerr=thinnlateralstddev, fmt='none',linewidth = 6, ecolor='#90EE90',capsize=8)
+
+        plt.plot(range(len(thickFlatMean)), [thickflataverage] * len(thickFlatMean), color='dodgerblue', linestyle='--', linewidth=6, label=f'Flat average: 3 mm: {thickflataverage:.2f}' fr'$\pm$' f'{thickflatstd:.2f} & 2 mm: ' f'{thinnflataverage:.2f}' fr'$\pm$' f'{thinnflatstd:.2f}')
+        plt.plot(range(len(thickFlatMean), len(thickFlatMean)+len(thinnFlatMean)), [thinnflataverage] * len(thinnFlatMean), linewidth=6, color='dodgerblue', linestyle='--')
+
+        plt.plot(range(len(thicklateralMean)), [thicklateralaverage] * len(thicklateralMean), color='crimson', linewidth=6,linestyle='--', label=f'Vertical average: 3 mm: {thicklateralaverage:.2f}' fr'$\pm$' f'{thicklateralstd:.2f} & 2 mm: ' f'{thinnlateralaverage:.2f}' fr'$\pm$' f'{thinnlateralstd:.2f}') 
+        plt.plot(range(len(thicklateralMean), len(thicklateralMean)+len(thinnlateralMean)), [thinnlateralaverage] * len(thinnlateralMean), color='crimson', linewidth=6, linestyle='--')
+        
+        plt.xlabel('Crystal index')
+        plt.ylabel('Light yield / ph/MeV')
+        plt.ylim(0)
+        plt.legend()
+        plt.grid(True)
+        plt.tight_layout()
+        plt.savefig("lightyield_comparison.pdf", format="pdf", dpi=300)
+        plt.show()
 if __name__ == "__main__":
     main()
